3b462b34aef83db4f8ab27d0da71ebedcd5ebbcc
bsbgaming <tytoti12@gmail.com>
2025-05-24 15:14:05 +0700

    Add files via upload

---
+++ web_mysqli/index.php
@@ -0,0 +1,38 @@
+<IDOCTYPE html>
+<html lang-"en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale-1.0">
+    <title>Web phu kien dien thoạl</title>
+    <link rel="stylesheet" type-"text/css" href="css/style.css">
+</head>
+<body>
+    <div class-"wrapper">
+        <div class="header"></div>
+        <div class="menu">
+            <ul class="list_menu">
+                <li><a href="index.php">Trang chủ</a></li>
+                <li><a href="#">Danh mục sản phẩm</a></li>
+                <li><a href="#">Giỏ hàng</a></li>
+                <li><a href="#">Tin tức</a></li>
+                <li><a href="#">Liên hệ</a></li>
+            </ul>
+    </div>
+    <div id="main">
+
+        <div class="sidebar">
+            <p>Sidebar</p>
+        </div>
+        <div class="maincontent">
+            <p>Main content</p>
+        </div>
+
+        </div>
+        <div class="clear"></div>
+        <div class="footer">
+            <p>footer</p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
